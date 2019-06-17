203d8214dfbd82c74c083ef1456bf9e00f6594df
SireeshaKeesara <skntc@mail.umkc.edu>
2019-06-16 21:24:10 -0700

    icp4

---
+++ icp4/source code/icp4/survived.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pathlib import Path
+
+train_df = pd.read_csv(Path('./train.csv'))
+x = train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(x)
